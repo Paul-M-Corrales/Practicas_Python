c831d3f10c210640f5d2eaf315d22204ff420d25
paul corrales <paulmatiascorrales@gmail.com>
2023-03-31 12:17:43 -0300

    modifico ejercicio

--- ejercicios_En_Clase/solicitud_De_Numeros.py
+++ ejercicios_En_Clase/solicitud_De_Numeros.py
@@ -3,5 +3,5 @@ cancelar = ""
 while cancelar != "exit()":
     numeros.append(int(input("Ingrese un numero entero"))) 
     cancelar = input("Desea detener el programa?? Escriba exit() si no escriba no").lower()
-
-print(sum(numeros))
+print(f"la suma parcial es de {numeros}")
+print(f"la suma total es de {sum(numeros)}")
