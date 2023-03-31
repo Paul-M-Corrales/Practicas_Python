67b1d14602cad2445ef490b5ab45226577256d84
paul corrales <paulmatiascorrales@gmail.com>
2023-03-31 11:49:03 -0300

    subo cancion me gustas tu de manu chao

---
+++ ejercicios_En_Clase/me_Gustas_ManuChao.py
@@ -0,0 +1,4 @@
+parrafos_Cancion = ["¿Qué horas son, mi corazón?", "(Te lo dije bien clarito)", "(Permanece a la escucha)", "(Permanece a la escucha)", "(Doce de la noche en La Habana, Cuba)", "(Once de la noche en San Salvador, El Salvador)", "(Once de la noche en Managua, Nicaragua)", "Me gustan los aviones, me gustas tú", "Me gusta viajar, me gustas tú", "Me gusta la mañana, me gustas tú", "Me gusta el viento, me gustas tú", "Me gusta soñar, me gustas tú", "Me gusta la mar, me gustas tú"]
+
+for cancion in parrafos_Cancion:
+    print(cancion)
